d6a300b01af8ab27faa8cf44516ce0f398787981
blacklotus <595272421@com>
2021-12-22 10:19:34 +0800

    地图基础数据增删改查

--- src/pages/map/map_maintain.vue
+++ src/pages/map/map_maintain.vue
@@ -404,6 +404,7 @@ export default {
           this.update_data.area_id = this.area_selected[0].id;
           this.update_data.type_id = this.type_selected[0].id;
           this.update_data.item_id = data.id;
+          
           break;
       }
       this.update_window = true;
